23a92f5ce1ee41175b28b2f0b8c6f4da692ee524
Sonic <142159825+sonicc97@users.noreply.github.com>
2024-08-04 15:28:28 +0200

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float range = 100;
+    private GM gm;
+
+    public Material[] colors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reposition();
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Reposition()
+    {
+        //return;
+        Vector3 pos = new Vector3(0, 6, 0);
+        pos.x = Random.Range(-range, range);
+        pos.z = Random.Range(-range, range);
+        transform.position = pos;
+    }
+
+    public void NextStage(int stage)
+    {
+        Reposition();
+        GetComponent<MeshRenderer>().material = colors[stage];
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Gun g = other.GetComponent<Gun>();
+
+        if (g)
+        {
+            gameObject.SetActive(false);
+            gm.CoinCollected();
+        }
+    }
+}
